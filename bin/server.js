f26d8023636f8ba21b14c08bc1d467a5019ab9ae
hitmen798 <124578265+hitmen798@users.noreply.github.com>
2024-02-22 14:12:11 +0100

    123

---
+++ bin/server.js
@@ -0,0 +1,37 @@
+const app = require('../app')
+const db = ('mongodb+srv://hitmen798:Hitmen47@cluster0.gwowun6.mongodb.net/?retryWrites=true&w=majority')
+const PORT = process.env.PORT || 3005
+
+const gracefulShutdown = async () => {
+  console.log('Server is gracefully shutting down...')
+
+  try {
+    await db.close()
+    console.error('Database connection closed.')
+  } catch (err) {
+    console.error('Error closing database connection:', err.message)
+  }
+
+  process.exitCode = 0 // Успішний вихід
+}
+
+// Обробка помилок при запуску сервера
+try {
+  const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+  })
+
+  // Обробка завершення роботи сервера
+  server.on('close', () => {
+    console.log('Server closed')
+    // Тут ви також можете викликати додаткові дії перед закриттям програми
+    process.exit()
+  })
+
+  // Обробка сигналів завершення роботи
+  process.on('SIGINT', gracefulShutdown)
+  process.on('SIGTERM', gracefulShutdown)
+} catch (err) {
+  console.error('Error starting the server:', err.message)
+  process.exitCode = 1 // Невдалений вихід
+}
