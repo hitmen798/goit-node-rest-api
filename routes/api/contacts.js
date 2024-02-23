95b4ff251c5c1c984fe10c5d57e8e72ef5548cbf
hitmen798 <124578265+hitmen798@users.noreply.github.com>
2024-02-23 23:25:15 +0100

    123

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,46 @@
-const express = require('express')
-const router = express.Router()
-const contactsController = require('./../../controllers/contacts')
-const validate = require('./validation')
+const express = require("express");
+const {
+  addValid,
+  updateValid,
+  updateFavoriteValid,
+} = require("../../middlewares/contactsValidation");
+const isValidId = require("../../middlewares/isValidId");
+const authenticate = require("../../middlewares/authenticate");
+const userVerification = require("../../middlewares/userVerification");
+const contactsCtrl = require("../../controllers/contacts.js");
 
-router
-  .get('/', contactsController.get)
-  .post('/', validate.createContact, contactsController.create)
+const router = express.Router();
 
-router
-  .get('/:id', contactsController.getById)
-  .delete('/:id', contactsController.remove)
-  .put('/:id', validate.updateContact, contactsController.update)
+router.get("/", authenticate, (req, res) => {
+  console.log("GET /contacts route hit");
+  contactsCtrl.listContactsForUser(req, res);
+});
 
-module.exports = router
+router.get("/:contactId", authenticate, isValidId, (req, res) => {
+  console.log("GET /contacts/:contactId route hit");
+  contactsCtrl.getContactById(req, res);
+});
+
+router.post("/", authenticate, addValid, (req, res) => {
+  console.log("POST /contacts route hit");
+  contactsCtrl.addContact(req, res);
+});
+
+router.delete("/:contactId", authenticate, userVerification, isValidId, (req, res) => {
+  console.log("DELETE /contacts/:contactId route hit");
+  contactsCtrl.removeContact(req, res);
+});
+
+router.put("/:contactId", authenticate, userVerification, isValidId, updateValid, (req, res) => {
+  console.log("PUT /contacts/:contactId route hit");
+  contactsCtrl.updateContact(req, res);
+});
+
+router.patch("/:contactId/favorite", authenticate, userVerification, isValidId, updateFavoriteValid, (req, res) => {
+  console.log("PATCH /contacts/:contactId/favorite route hit");
+  contactsCtrl.updateStatusContact(req, res);
+});
+
+module.exports = router;
+
+
---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const app = require("./app");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect('mongodb+srv://hitmen798:Hitmen47@cluster0.gwowun6.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful on port 3000");
+  })
+  .catch((err) => {
+    console.error(err.message);
+    process.exit(1);
+  });
+
+process.on('SIGINT', async () => {
+  try {
+    await mongoose.connection.close();
+    console.log('MongoDB connection closed.');
+    process.exit(0);
+  } catch (err) {
+    console.error('Error closing MongoDB connection:', err.message);
+    process.exit(1);
+  }
+});
+//  Art 
